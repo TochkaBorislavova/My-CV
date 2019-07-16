18ab0a1afbcb6b497f7266ee87764463b1eedbaf
Bilyana Borislavova <happy.bozanko@gmail.com>
2019-07-16 15:32:20 +0300

    Added gh-pages

---
+++ build/precache-manifest.22cb96b4cbcc09b30ffa7c6ed5e4954e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8135996561b57190d657c88a809a141d",
+    "url": "/My-CV/index.html"
+  },
+  {
+    "revision": "c0c54201511ab5bdcdb3",
+    "url": "/My-CV/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/My-CV/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c0c54201511ab5bdcdb3",
+    "url": "/My-CV/static/js/main.ec150d15.chunk.js"
+  },
+  {
+    "revision": "7fe640c044fe115293d3",
+    "url": "/My-CV/static/js/runtime~main.368478a5.js"
+  }
+]);
