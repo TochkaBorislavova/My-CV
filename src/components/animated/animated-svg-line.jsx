420af92c1f6c25c0bd27a3d7a753f59037a3f7e1
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2020-12-24 18:10:57 +0200

    Fixes

--- src/components/animated/animated-svg-line.jsx
+++ src/components/animated/animated-svg-line.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const AnimatedSvgLine = ({ children }) => (
     <section className="animated-svg-line">
         <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-          <rect class="shape" height="60" width="320" />
+          <rect className="shape" height="60" width="320" />
         </svg>
         <article className="animated-svg-line-text">
             {children}
