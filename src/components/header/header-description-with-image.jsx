10e446a5942407558a9d93f25dd184101f9cdbe1
BilyanaBorislavova <BilyanaBorislavova@users.noreply.github.com>
2020-12-29 15:54:12 +0200

    Removed horrible components

--- src/components/header/header-description-with-image.jsx
+++ src/components/header/header-description-with-image.jsx
@@ -1,8 +1,6 @@
 import './header-description-with-image.scss';
 
 import React from 'react';
-import ContactCta from '../ctas/contact-cta';
-import ContactInfo from '../contact-info/contact-info';
 
 // const imageSrc = 'https://instagram.fsof9-1.fna.fbcdn.net/v/t51.2885-15/e35/109568396_162785035386492_3555223871628390635_n.jpg?_nc_ht=instagram.fsof9-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=PxNYZdwBP-EAX_uVLRD&oh=a0f6fa51a098033853d333a01337c62a&oe=5F64770D';
 const imageSrc = 'https://iupac.org/wp-content/uploads/2018/05/default-avatar.png';
@@ -23,9 +21,6 @@ const HeaderDescriptionWithImage = () => (
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga odit architecto voluptate, dignissimos, consectetur dolore voluptatibus ex repellat ad velit atque aliquid iure corrupti dicta perferendis quos? Sint, repellat ad.
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error adipisci in aperiam ad natus atque beatae tenetur perferendis illo maiores officiis repellat veritatis, assumenda, nam doloribus maxime impedit nulla explicabo.
             </p>
-            <ContactCta>
-                <ContactInfo />
-            </ContactCta>
         </section>
     </header>
 );
